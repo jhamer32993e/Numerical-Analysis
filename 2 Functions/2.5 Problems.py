8f5c4c9e6e0e15ace60c640e95809fb0b6d6521e
Joe <60753704+jhamer32993e@users.noreply.github.com>
2026-02-22 21:51:51 +0000

    Wk 2 Problems Complete

---
+++ 2 Functions/2.5 Problems.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+
+
+# Pi approximation using Taylor series of 1/(1+x), then x -> x^2, then integrate
+# the resulting function = arctan(x), then pi = 4arctan(1)
+def pi_approx(n, x=1):
+    if n < 0:
+        raise ValueError("n must be at least 0")
+    approximation = 0
+    for i in range((n + 1) // 2):
+        approximation += x ** (2 * i + 1) * (-1) ** i / (2 * i + 1)
+    approximation *= 4
+    return approximation
+
+
+print(pi_approx(10000000))
