626f1a1fe7e6a86cc381f888cea6b2071d9d5292
joseph hamer <josephmhamer@gmail.com>
2026-02-25 22:05:29 +0000

    Week 3 Complete

--- 3 Roots 1/3.3 Fixed Point Iteration.py
+++ 3 Roots 1/3.3 Fixed Point Iteration.py
@@ -7,3 +7,19 @@ for i in range(10):
     x1 = np.cos(x1)
 print(x1)
 print("Estimated solution of cos(x)-x=0 is:", x1)
+print("-" * 80)
+
+
+def fixed_point(g, x0, tol, n):
+    y = 0
+    for i in range(n):
+        y = x0
+        x0 = g(x0)
+        if np.abs(y - x0) < tol:
+            return x0
+            break
+    return ValueError("Iteration has not converged")
+
+
+g1 = lambda x: np.cos(x)
+print(fixed_point(g1, 2, 1e-8, 100))
