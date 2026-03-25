a7234a35ff3676d443f138f1ccd2ce1aabc656c5
joseph hamer <josephmhamer@gmail.com>
2026-03-25 19:29:43 +0000

    7.7 Done

--- 7 ODEs 1/7.1 Euler'sMethod.py
+++ 7 ODEs 1/7.1 Euler'sMethod.py
@@ -37,3 +37,57 @@ plt.legend(loc="upper right")
 plt.xlim(0, max(x) + 0.5)
 plt.ylim(-1, 7)
 plt.show()
+
+print("- " * 40)
+
+
+# 7.5
+def Euler1D(f, x0, t0, tmax, dt):
+    N = round((tmax - t0) / dt)
+    dt = (tmax - t0) / N
+
+    t = np.linspace(t0, tmax, N + 1)
+    x = np.zeros(len(t))
+
+    x[0] = x0
+    for n in range(N):
+        x[n + 1] = x[n] + dt * f(x[n], t[n])
+
+    return x, t
+
+
+# 7.6
+f = lambda x, t: -1 / 3 * x + np.sin(t)
+xExact = lambda t: 0.1 * (19 * np.exp(-t / 3) + 3 * np.sin(t) - 9 * np.cos(t))
+x0 = 1
+t0 = 0
+tmax = 10
+dt = 0.5
+x, t = Euler1D(f, x0, t0, tmax, dt)
+plt.plot(t, x, "b-", label="Euler")
+
+t_highres = np.linspace(t0, tmax, 100)
+plt.plot(t_highres, xExact(t_highres), "r--", label="Exact")
+plt.legend()
+plt.grid()
+plt.show()
+
+print("- " * 40)
+
+
+# 7.7
+def EulerError(f, x0, t0, tmax, hFactor, xExact):
+    dt = hFactor ** (-np.linspace(0, 4, 50))
+    errors = np.zeros_like(dt)
+    for i in range(len(dt)):
+        xApprox, t = Euler1D(f, x0, t0, tmax, dt[i])
+        errors[i] = np.max(np.abs(xExact(t) - xApprox))
+
+    plt.loglog(dt, errors)
+    plt.xlabel("Step size")
+    plt.ylabel("Maximum error")
+    plt.grid()
+    plt.show()
+
+
+EulerError(f, x0, t0, tmax, 10, xExact)
