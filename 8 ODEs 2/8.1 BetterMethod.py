f5cf68a9af75cc49c9ca62e748593d8e63380cde
joseph hamer <josephmhamer@gmail.com>
2026-04-13 23:06:09 +0100

    Up to 8.3 done

--- 8 ODEs 2/8.1 BetterMethod.py
+++ 8 ODEs 2/8.1 BetterMethod.py
@@ -38,13 +38,13 @@ def ode_test(f, x0, t0, tmax, dt, c):
     x = np.zeros(len(t))  # set up the x
     x[0] = x0  # initial condition
     for n in range(len(t) - 1):
-        m_n = f(t[n], x[n])
+        m_n = f(x[n], t[n])
         x_n_plus_half = x[n] + dt / 2 * m_n
-        m_n_plus_half = f(t[n] + dt / 2, x_n_plus_half)
+        m_n_plus_half = f(x_n_plus_half, t[n] + dt / 2)
         x_n_plus_half_star = x[n] + dt / 2 * m_n_plus_half
-        m_n_plus_half_star = f(t[n] + dt / 2, x_n_plus_half_star)
+        m_n_plus_half_star = f(x_n_plus_half_star, t[n] + dt / 2)
         x_n_plus_1_star = x[n] + dt * m_n_plus_half_star
-        m_n_plus_1_star = f(t[n] + dt, x_n_plus_1_star)
+        m_n_plus_1_star = f(x_n_plus_1_star, t[n] + dt)
         estimate_of_slope = (
             c[0] * m_n
             + c[1] * m_n_plus_half
@@ -52,16 +52,16 @@ def ode_test(f, x0, t0, tmax, dt, c):
             + c[3] * m_n_plus_1_star
         )
         x[n + 1] = x[n] + dt * estimate_of_slope
-    return t, x
+    return x, t
 
 
-f = lambda t, x: -(1 / 3.0) * x + np.sin(t)
+f = lambda x, t: -(1 / 3.0) * x + np.sin(t)
 exact = lambda t: (1 / 10.0) * (19 * np.exp(-t / 3) + 3 * np.sin(t) - 9 * np.cos(t))
 
 x0 = 1  # initial condition
 t0 = 0  # initial time
 tmax = 3  # max time
-dt = 10.0 ** (-np.arange(1, 5, 1))
+dt = 10.0 ** (-np.linspace(0, 1.5, 10))
 C = [
     [1, 0, 0, 0],
     [0, 1, 0, 0],
@@ -86,14 +86,18 @@ for i in range(len(C)):
         err_euler[n] = np.max(np.abs(xeuler - exact(t)))
         xmidpoint, t = Midpoint1D(f, x0, t0, tmax, dt[n])
         err_midpoint[n] = np.max(np.abs(xmidpoint - exact(t)))
-        t, xtest = ode_test(f, x0, t0, tmax, dt[n], C[i])
+        xtest, t = ode_test(f, x0, t0, tmax, dt[n], C[i])
         err_ode_test[n] = np.max(np.abs(xtest - exact(t)))
 
     plt.loglog(dt, err_euler, "r*-", dt, err_midpoint, "b*-", dt, err_ode_test, "k*-")
     plt.xlabel("Step Size")
     plt.ylabel("Error")
     plt.grid()
-    plt.title(f"{i+1}th Set of c values")
+    plt.title(f"{i+1}-th Set of c values")
     plt.legend(["euler", "midpoint", "test method"])
     plt.show()
 # Slope gives the order of the error
+
+
+f = lambda x, t: -0.5*x
+print(Midpoint1D(f, 6, 0, 6, 1))
---
+++ 8 ODEs 2/8.2 Runge-KuttaMethod.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# 8.5
+def RK4_1D(f, x0, t0, tmax, dt):
+    N = round((tmax - t0) / dt)
+    dt = (tmax - t0) / N
+    t = np.linspace(t0, tmax, N + 1)
+    x = np.zeros(len(t))
+    x[0] = x0
+    for n in range(N):
+        m_n = f(x[n], t[n])
+        x_n_plus_half = x[n] + dt / 2 * m_n
+        m_n_plus_half = f(x_n_plus_half, t[n] + dt / 2)
+        x_n_plus_half_star = x[n] + dt / 2 * m_n_plus_half
+        m_n_plus_half_star = f(x_n_plus_half_star, t[n] + dt / 2)
+        x_n_plus_1_star = x[n] + dt * m_n_plus_half_star
+        m_n_plus_1_star = f(x_n_plus_1_star, t[n] + dt)
+        estimate_of_slope = (
+            1 / 6 * m_n
+            + 2 / 6 * m_n_plus_half
+            + 2 / 6 * m_n_plus_half_star
+            + 1 / 6 * m_n_plus_1_star
+        )
+        x[n + 1] = x[n] + dt * estimate_of_slope
+    return x, t
+
+
+# 8.6
+def RK4Error(f, x0, t0, tmax, hFactor, xExact):
+    dt = hFactor ** (-np.linspace(0, 4, 50))
+    errors = np.zeros_like(dt)
+    for i in range(len(dt)):
+        xApprox, t = RK4_1D(f, x0, t0, tmax, dt[i])
+        errors[i] = np.max(np.abs(xExact(t) - xApprox))
+
+    plt.loglog(dt, errors)
+    plt.xlabel("Step size")
+    plt.ylabel("Maximum error")
+    plt.grid()
+    plt.show()
+
+
+# 8.7
+def RK4(F, x0, t0, tmax, dt):
+    N = round((tmax - t0) / dt)
+    dt = (tmax - t0) / N
+
+    t = np.linspace(t0, tmax, N + 1)
+
+    x = np.zeros((len(t), len(x0)))
+    x[0, :] = x0
+    for n in range(N):
+        m_n = F(x[n, :], t[n])
+        x_n_plus_half = x[n, :] + dt / 2 * m_n
+        m_n_plus_half = F(x_n_plus_half, t[n] + dt / 2)
+        x_n_plus_half_star = x[n, :] + dt / 2 * m_n_plus_half
+        m_n_plus_half_star = F(x_n_plus_half_star, t[n] + dt / 2)
+        x_n_plus_1_star = x[n, :] + dt * m_n_plus_half_star
+        m_n_plus_1_star = F(x_n_plus_1_star, t[n] + dt)
+        estimate_of_slope = (
+            1 / 6 * m_n
+            + 2 / 6 * m_n_plus_half
+            + 2 / 6 * m_n_plus_half_star
+            + 1 / 6 * m_n_plus_1_star
+        )
+        x[n + 1, :] = x[n, :] + dt * estimate_of_slope
+    return x, t
+
+
+t = [-2, -2.5, -3]
+print(6*np.exp(t))
